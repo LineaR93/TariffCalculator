1b6298a4c9d0497483f5b73c66ceb2114a6bd79b
LineaR93 <155578865+LineaR93@users.noreply.github.com>
2024-05-25 16:51:44 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,29 @@
-// Configurazione di Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBtNsDjH7oivnrDU7Bi_5fR62ksa4-2N9Q",
+    authDomain: "petsitter-d69ed.firebaseapp.com",
+    projectId: "petsitter-d69ed",
+    storageBucket: "petsitter-d69ed.appspot.com",
+    messagingSenderId: "119772151716",
+    appId: "1:119772151716:web:2fd12bfab1972bb3354b31",
+    measurementId: "G-9J4RWJLTQ2"
 };
 
-// Inizializza Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
 function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             localStorage.setItem('isLoggedIn', true);
             window.location.href = 'index.html';
@@ -26,7 +34,7 @@ function login() {
 }
 
 function checkLogin() {
-    firebase.auth().onAuthStateChanged((user) => {
+    onAuthStateChanged(auth, (user) => {
         if (user) {
             document.querySelector('.icon-container').style.display = 'block';
         } else {
@@ -36,7 +44,7 @@ function checkLogin() {
 }
 
 function logout() {
-    firebase.auth().signOut().then(() => {
+    signOut(auth).then(() => {
         localStorage.removeItem('isLoggedIn');
         window.location.href = 'login.html';
     });
