c2c65f530181ce5aa99629aecaa965ec7d62662a
LineaR93 <155578865+LineaR93@users.noreply.github.com>
2024-05-25 16:49:43 +0200

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,4 +1,8 @@
-// Configurazione di Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getFirestore, collection, addDoc, orderBy, query, limit, getDocs } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBtNsDjH7oivnrDU7Bi_5fR62ksa4-2N9Q",
     authDomain: "petsitter-d69ed.firebaseapp.com",
@@ -9,15 +13,16 @@ const firebaseConfig = {
     measurementId: "G-9J4RWJLTQ2"
 };
 
-// Inizializza Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-function backupData() {
+export function backupData() {
     const reservations = JSON.parse(localStorage.getItem('reservations')) || [];
     const appointments = JSON.parse(localStorage.getItem('appointments')) || [];
 
-    db.collection("backups").add({
+    addDoc(collection(db, "backups"), {
         reservations: reservations,
         appointments: appointments,
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
@@ -28,16 +33,16 @@ function backupData() {
     });
 }
 
-function restoreData() {
-    db.collection("backups").orderBy("timestamp", "desc").limit(1).get()
-        .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                const data = doc.data();
-                localStorage.setItem('reservations', JSON.stringify(data.reservations));
-                localStorage.setItem('appointments', JSON.stringify(data.appointments));
-                alert('Ripristino dati effettuato con successo!');
-            });
-        }).catch(error => {
-            console.error('Errore durante il ripristino:', error);
+export function restoreData() {
+    const q = query(collection(db, "backups"), orderBy("timestamp", "desc"), limit(1));
+    getDocs(q).then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            const data = doc.data();
+            localStorage.setItem('reservations', JSON.stringify(data.reservations));
+            localStorage.setItem('appointments', JSON.stringify(data.appointments));
+            alert('Ripristino dati effettuato con successo!');
         });
+    }).catch(error => {
+        console.error('Errore durante il ripristino:', error);
+    });
 }
