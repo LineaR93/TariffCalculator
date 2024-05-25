3dc980b609ad551ad465e99628473df002b2fb40
LineaR93 <155578865+LineaR93@users.noreply.github.com>
2024-05-25 18:44:29 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,26 +1,17 @@
-function saveTariffs() {
-    const dogTariff1 = parseFloat(document.getElementById('dogTariff1').value);
-    const dogTariff2 = parseFloat(document.getElementById('dogTariff2').value);
-    const dogTariff3 = parseFloat(document.getElementById('dogTariff3').value);
-    const catTariff1 = parseFloat(document.getElementById('catTariff1').value);
-    const catTariff2 = parseFloat(document.getElementById('catTariff2').value);
-    const catTariff3 = parseFloat(document.getElementById('catTariff3').value);
+document.addEventListener('DOMContentLoaded', loadTariffs);
 
-    if (!isNaN(dogTariff1) && !isNaN(dogTariff2) && !isNaN(dogTariff3) &&
-        !isNaN(catTariff1) && !isNaN(catTariff2) && !isNaN(catTariff3)) {
-        localStorage.setItem('dogTariff1', dogTariff1);
-        localStorage.setItem('dogTariff2', dogTariff2);
-        localStorage.setItem('dogTariff3', dogTariff3);
-        localStorage.setItem('catTariff1', catTariff1);
-        localStorage.setItem('catTariff2', catTariff2);
-        localStorage.setItem('catTariff3', catTariff3);
-        alert('Tariffe salvate con successo!');
-    } else {
-        alert('Per favore, inserisci tutte le tariffe correttamente.');
-    }
+function saveTariffs() {
+    localStorage.setItem('dogTariff1', document.getElementById('dogTariff1').value);
+    localStorage.setItem('dogTariff2', document.getElementById('dogTariff2').value);
+    localStorage.setItem('dogTariff3', document.getElementById('dogTariff3').value);
+    localStorage.setItem('catTariff1', document.getElementById('catTariff1').value);
+    localStorage.setItem('catTariff2', document.getElementById('catTariff2').value);
+    localStorage.setItem('catTariff3', document.getElementById('catTariff3').value);
+
+    alert('Tariffe salvate con successo!');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function loadTariffs() {
     if (localStorage.getItem('dogTariff1')) {
         document.getElementById('dogTariff1').value = localStorage.getItem('dogTariff1');
     }
@@ -39,4 +30,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('catTariff3')) {
         document.getElementById('catTariff3').value = localStorage.getItem('catTariff3');
     }
-});
+}
